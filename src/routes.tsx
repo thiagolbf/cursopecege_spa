d40482e6732632062e978276d8b373c103310ffa
Thiago Barcelos <thiagolbf@hotmail.com>
2024-04-23 21:31:05 -0300

    feat: autorização do painel de admin

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,30 +1,48 @@
-import { createBrowserRouter } from 'react-router-dom'
-import { Home } from './pages/Home'
-import { Pets } from './pages/Pets'
-import { PetDetails } from './pages/PetDetails'
+import { createBrowserRouter } from "react-router-dom";
+import { Home } from "./pages/Home";
+import { Pets } from "./pages/Pets";
+import { PetDetails } from "./pages/PetDetails";
+import { Shelter } from "./pages/Admin/Shelter";
+import { PetList } from "./pages/Admin/PetList";
+import { AuthHOC } from "./components/common/AuthHOC/AuthHOC";
 
 const router = createBrowserRouter([
   {
-    path: '/',
+    path: "/",
     element: <Home />,
   },
   {
-    path: '/pets',
+    path: "/pets",
     children: [
       {
         index: true,
         element: <Pets />,
       },
       {
-        path: '/pets/:id',
+        path: "/pets/:id",
         element: <PetDetails />,
       },
     ],
   },
   {
-    path: '/admin',
-    element: <h1>Admin</h1>,
+    path: "/admin",
+    children: [
+      {
+        index: true,
+        element: <Shelter />,
+      },
+      {
+        path: "/admin/pets",
+        element: <AuthHOC />,
+        children: [
+          {
+            index: true,
+            element: <PetList />,
+          },
+        ],
+      },
+    ],
   },
-])
+]);
 
-export default router
+export default router;
