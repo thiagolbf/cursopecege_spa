027b5f600b41926760ea8f28f3ddc7a3c8ffae51
Thiago Barcelos <thiagolbf@hotmail.com>
2024-04-23 22:15:55 -0300

    feat: input com funcionalidade do fowardref

--- src/pages/Admin/Shelter/Shelter.tsx
+++ src/pages/Admin/Shelter/Shelter.tsx
@@ -1,5 +1,18 @@
+import { Button } from "../../../components/common/Button";
+import { Input } from "../../../components/common/Input";
 import { Panel } from "../../../components/layout/Panel";
 
+import styles from "./Shelter.module.css";
+
 export function Shelter() {
-  return <Panel>Formulário de abrigo</Panel>;
+  function submit() {}
+
+  return (
+    <Panel>
+      <form className={styles.container} onSubmit={submit}>
+        <Input label="Nome" />
+        <Button type="submit"> Salvar dados</Button>
+      </form>
+    </Panel>
+  );
 }
